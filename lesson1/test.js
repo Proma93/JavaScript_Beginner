0cc7cd4e41a7b26d2633e1b62c0dc596fb095770
Proma93 <ahmed.farjana93@gmail.com>
2022-04-06 20:56:16 +0200

    lesson1

---
+++ lesson1/test.js
@@ -0,0 +1,15 @@
+let str = "Apple, Banana, Kiwi";
+let part = str.substring(7, 13);
+console.log(part);
+
+let text = "Please visit Microsoft!";
+let newText = text.replace("Microsoft", "W3Schools");
+console.log(newText);
+
+let text1 = "Hello World!";
+let text2 = text1.toUpperCase();
+console.log(text2);
+
+let text3 = "Hello World!";       // String
+let text4 = text3.toLowerCase();
+console.log(text4);
